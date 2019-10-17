1d3304913d88dc2c71776b0256ca967064520b68
Bala_Srinivasan <balatriplee@gmail.com>
2019-10-17 18:23:10 +0530

    deletion is done

---
+++ deletion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def deletion(a,b):
+    print('The sum of {} and {} is {}'.format(a,b,str(int(a)-int(b))))
+
+if __name__ == '__main__':
+    print('Deletion operation')
+    sum(input('Enter first no. - '), input('Enter second no. - '))
