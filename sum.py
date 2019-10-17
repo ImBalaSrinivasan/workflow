af0f281524d6f81cc115509f18aec7fd1de08911
Bala_Srinivasan <balatriplee@gmail.com>
2019-10-17 15:21:37 +0530

    addition feature

---
+++ sum.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def sum(a,b):
+    print('The sum of {} and {} is {}'.format(a,b,str(int(a)+int(b))))
+
+if __name__ == '__main__':
+    print('Addition operation')
+    sum(input('Enter first no. - '), input('Enter second no. - '))
