b4b6493dbe0fa123ca9e113970839a47a4d204b7
Bala_Srinivasan <balatriplee@gmail.com>
2019-10-19 18:18:52 +0530

    deleted div

--- division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def multi(a,b):
-    print('The sum of {} and {} is {}'.format(a,b,str(int(a) * int(b))))
-
-if __name__ == '__main__':
-    print('Multiplication operation')
-    multi(input('Enter first no. - '), input('Enter second no. - '))
-    #temp
